9ba1f2177cc4644059be7a9c8d8060b2782c1186
Jon Thomas <jon.thomas@bunn.com>
2020-09-04 15:08:08 -0500

    Initial commit

---
+++ styles/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [],
+    theme: {
+        extend: {
+            rotate: {
+                '135': '135deg',
+                '270': '270deg'
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
+}
